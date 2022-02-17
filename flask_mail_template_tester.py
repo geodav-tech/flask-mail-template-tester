4a1420c926b66f3a4c0050395feec79ca32f5873
Shawn Pacarar <spacarar@hotmail.com>
2022-02-17 14:13:04 -0700

    working prototype

--- flask_mail_template_tester.py
+++ flask_mail_template_tester.py
@@ -1,20 +1,22 @@
 from __future__ import absolute_import, division, print_function
 import os
 import click
+from flask import render_template
 from flask.cli import with_appcontext
+from flask_mail import Mail, Message
 
 @click.command('template-test')
 @click.argument('templatename')
-@click.option('-d', '--data', 'dataFile', help="data file must set a template_data object to be given to the template. $TEMPLATE_DATA_FILE or cwd()/template_test_data.py")
+@click.option('-d', '--data', 'dataFile', help="data file must set a template_data object to be given to the template. $TEMPLATE_DATA_FILE or cwd()/templates/test_data.py")
 @click.option('-e', '--email', help="test recipient email. $TEMPLATE_EMAIL")
 @with_appcontext
 def test_template(templatename, dataFile, email):
     from flask.globals import _app_ctx_stack
     app = _app_ctx_stack.top.app
-    mail = app.mail
+    mail = Mail(app)
 
     # data file directory or default to a data.py file
-    dataFile = dataFile or os.getenv('TEMPLATE_DATA_FILE') or os.path.join(os.getcwd(), 'template_test_data.py')
+    dataFile = dataFile or os.getenv('TEMPLATE_DATA_FILE') or os.path.join(os.getcwd(), 'templates', 'test_data.py')
     email = email or os.getenv('TEMPLATE_EMAIL')
     if not email:
         raise "Email not provided to template tester. Pass -e or use TEMPLATE_EMAIL environment variable"
